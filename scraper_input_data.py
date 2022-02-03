9662317715c688aaec9bb476fc4cb6d3650053ed
danvisser <93989494+danvisser@users.noreply.github.com>
2022-02-03 11:04:02 +0000

    Add files via upload

---
+++ scraper_input_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+name_dict  = {
+    "Degenerate Ape Academy": {
+        "Twitter": "@DegenApeAcademy",
+        "Influencers": []
+    },
+    "Aurory": {
+        "Twitter": "@AuroryProject" ,
+        "Influencers": []
+    },
+    "SolPunks": {
+        "Twitter": "@PunksOnSolana",
+        "Influencers": []
+    },
+    "Sollamas": {
+        "Twitter": "@SollamasNFT",
+        "Influencers": []
+    },
+    "Solana Monkey Business": {
+        "Twitter": "@SolanaMBS",
+        "Influencers": []
+    }
+}
+
+# Collection ID
+# Sentiment
+# Polarity
+# TwitterID
+# Time
+# Tweet
+# Retweet
+# Comment
+# Likes
